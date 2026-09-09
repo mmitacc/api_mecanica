10c04a0136ad85fb8efcd5691cc7baff19624750
mmitacc <manuelmitacc@hotmail.com>
2026-09-09 14:44:25 -0500

    fix/usuario and delete refactorize auth

--- src/routes/usuario.route.ts
+++ src/routes/usuario.route.ts
@@ -6,45 +6,16 @@ import {
   putUsuarioPassword,
   deleteUsuario,
   registerUsuario,
-  loginUsuario,
 } from "../controllers/usuario.controller";
 import { authorize } from "../middlewares/authorize.middleware";
-import { verifyToken } from "../middlewares/auth.middleware";
 
 const router = Router();
 
-router.post(
-  "/login",
-  loginUsuario,
-  /*  
-    #swagger.tags = ['Auth']
-    #swagger.summary = 'logueo a un Usuario y genera su token'
-    #swagger.description = 'El token del usuario'
-    #swagger.requestBody = {
-        description: 'Ingresar sus datos para poder loguearse',
-        required: true,
-        content: {
-            "application/json": {
-                schema: {
-                    type: 'object',
-                    properties: {                
-                      "email": {type: 'string', example: "jp@mail.com"},
-                      "password": {type: 'string', example: "Password123"},
-                    }
-                }
-            }
-        }
-    }        
-  */
-);
-
 router.get(
   "/",
-  verifyToken,
   authorize("DUEÑO"),
   getUsuarios,
   /*  
-    #swagger.security = [{ "bearerAuth": [] }] 
     #swagger.tags = ['Usuario']
     #swagger.summary = 'Obtener todos los Usuarios'
     #swagger.description = 'Retorna toda la lista de usuarios con todos sus campos'
@@ -53,11 +24,9 @@ router.get(
 
 router.get(
   "/:id",
-  verifyToken,
   authorize("DUEÑO"),
   getById,
   /*  
-    #swagger.security = [{ "bearerAuth": [] }] 
     #swagger.tags = ['Usuario']
     #swagger.summary = 'Ubica un Usuario por su ID'
     #swagger.description = 'Retorna el usuario encontrado, con todos sus campos'
@@ -72,11 +41,9 @@ router.get(
 
 router.post(
   "/register",
-  verifyToken,
   authorize("DUEÑO"),
   registerUsuario,
   /* 
-    #swagger.security = [{ "bearerAuth": [] }] 
     #swagger.tags = ['Usuario']
     #swagger.tags = ['Auth']
     #swagger.summary = 'Registrar un Usuario nuevo'
@@ -104,10 +71,8 @@ router.post(
 
 router.put(
   "/password",
-  verifyToken,
   putUsuarioPassword,
   /*  
-    #swagger.security = [{ "bearerAuth": [] }] 
     #swagger.tags = ['Usuario']
     #swagger.summary = 'Actualiza el PASSWORD de un usuario logueado correctamente'
     #swagger.description = 'Retorna un mensaje de confirmación'
@@ -131,11 +96,9 @@ router.put(
 
 router.put(
   "/:id",
-  verifyToken,
   authorize("DUEÑO"),
   putUsuario,
   /*  
-    #swagger.security = [{ "bearerAuth": [] }] 
     #swagger.tags = ['Usuario']
     #swagger.summary = 'Actualiza uno ó más datos del Usuario'
     #swagger.description = 'Retorna el usuario actualizado'
@@ -167,11 +130,9 @@ router.put(
 
 router.delete(
   "/:id",
-  verifyToken,
   authorize("DUEÑO"),
   deleteUsuario,
   /*  
-    #swagger.security = [{ "bearerAuth": [] }] 
     #swagger.tags = ['Usuario']
     #swagger.summary = 'Elimina un Usuario'
     #swagger.description = 'Retorna el usuario eliminado, con todos sus campos'
